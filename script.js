610ddf8a22e5c88c22b6be76643c36b1e2386d46
ornella28 <=>
2020-10-15 16:44:09 +0200

    Added comments about git

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/*
+Steps for commit code into Github:
+1. git add .
+2. git commit -m "Any message"
+3. git push
+*/
+
 var number1 = 5 , number2 = 8;
 var sum = number1 + number2;
 var result = number1 + '' + "plus" + '' + number2 + '' + "är" + '' + sum;
