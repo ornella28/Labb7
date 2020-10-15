bb7925d9d75c5866fa31203d696366c6d9dd34d7
ornella28 <=>
2020-10-15 16:33:58 +0200

    Started with labb 7

---
+++ script.js
@@ -0,0 +1,12 @@
+var number1 = 5 , number2 = 8;
+var sum = number1 + number2;
+var result = number1 + '' + "plus" + '' + number2 + '' + "är" + '' + sum;
+
+
+console.log(number1+number2);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(result);
+
+
+
